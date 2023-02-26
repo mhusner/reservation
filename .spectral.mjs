c5ba579bf5b347f8db10e1649d2200237737385a
milan.husner@alef.com <milan.husner@alef.com>
2023-02-26 02:46:03 +0100

    rem old resources

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE3NzExMg/spectral.js?branch_slug=main&token=3d09ea87-009f-4472-b7db-a055ef0f69cb";
-export default { extends: ruleset };
