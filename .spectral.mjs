c70a2ceee8b3d911f85949d1e5ac7d32452d8691
husner.milan@gmail.com <husner.milan@gmail.com>
2023-02-26 03:03:46 +0100

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4NzEwNA/spectral.js?branch_slug=main&token=44c85a70-5589-41d7-9f54-0bef1b324375";
+export default { extends: ruleset };
